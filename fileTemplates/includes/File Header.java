be3a752d7008c6ef74c29c69c02637bea316b823
zhaoyu <zyazxr@139.com>
2019-09-19 11:12:31 +0800

    IU-2018.3.5 <zhaoy@DESKTOP-ZY Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, project.default.xml, rainbow_brackets.xml, Enum.java, Default copy.xml, Interface.java, lombok-plugin.xml, other.xml, Default.xml, applicationLibraries.xml, file.template.settings.xml, find.xml, ui.lnf.xml, File Header.java, log_highlighting.xml, filetypes.xml, keymap.xml, scala_config.xml, _@user_Darcula.icls, databaseDrivers.xml, github.xml, templates.xml, Ladies Night 2.icls, Default.xml, scala.xml, my_own.xml, laf.xml, KeyPromoterXStatistic.xml, .xml, Class.java, jdk.table.xml, Hevin.icls, Relax Your Eyes Green.xml, path.macros.xml, vcs.xml, databaseSettings.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+
+/**
+ * 〈一句话功能简述〉
+ * 〈${DESCRIPTION}〉
+ *
+ * @author ${USER}
+ * @create  ${DATE} ${TIME}${HOUR}
+ * @since 1.0.0
+ */
---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,17 @@
+ /*
+  * Copyright (C), 2015-${YEAR}, NARI TECH
+  * FileName: ${NAME}
+  * Author:   ${USER}
+  * Date:     ${DATE} ${TIME}${HOUR}:${MINUTE}
+  * Description: ${DESCRIPTION}
+  * History:
+  * <author>          <time>          <version>          <desc>
+  * 作者姓名           修改时间           版本号              描述
+  *
+  */
+
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+ 
+public class ${NAME} {
+}
